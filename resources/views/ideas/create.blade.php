65a062b32b18feeae9e3d85a9c4d6dd0e6011b71
TeDDzTeR99 <myrkalol99@gmail.com>
2026-05-16 19:17:21 +0300

    Request Validation Done

--- resources/views/ideas/create.blade.php
+++ resources/views/ideas/create.blade.php
@@ -6,6 +6,7 @@
                 <label for="description" class="block text-sm/6 font-medium text-white">Create New Idea</label>
                     <div class="mt-2">
                         <textarea id="description" name="description" rows="3" class="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"></textarea>
+                        <x-form.error name="description" />
                     </div>
                 <p class="mt-3 text-sm/6 text-gray-400">Write a few sentences about your new idea.</p>
             </div>
