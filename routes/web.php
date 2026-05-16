2fbc28842b6cf156c8213a5bb4e2aecdd236bc4f
TeDDzTeR99 <myrkalol99@gmail.com>
2026-05-16 19:03:01 +0300

    Controllers Done

--- routes/web.php
+++ routes/web.php
@@ -1,55 +1,28 @@
 <?php
 
-use App\Models\Idea;
+use App\Http\Controllers\IdeaController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/ideas', function () {
-    //$ideas = DB::table('ideas')->latest()->get();
-    $ideas = Idea::all();
-    return view('ideas.index', [
-        'ideas' => $ideas,
-    ]);
-});
 Route::view('/about', 'about');
 Route::view('/contact', 'contact');
+
+// Index
+Route::get('/ideas', [IdeaController::class, 'index']);
+
+//Create
+Route::get('/ideas/create', [IdeaController::class, 'create']);
+
 // Show
-Route::get('/ideas/{idea}', function (Idea $idea) {
+Route::get('/ideas/{idea}', [IdeaController::class, 'show']);
 
-    return view('ideas.show', [
-        'idea' => $idea,
-    ]);
-});
 // Edit
-Route::get('/ideas/{idea}/edit', function (Idea $idea) {
-
-    return view('ideas.edit', [
-        'idea' => $idea,
-    ]);
-});
+Route::get('/ideas/{idea}/edit', [IdeaController::class, 'edit']);
 
 // Update
-Route::patch('/ideas/{idea}', function (Idea $idea) {
+Route::patch('/ideas/{idea}', [IdeaController::class, 'update']);
 
-    $idea->update([
-        'description' => request('description'),
-    ]);
-
-    return redirect('/ideas/' . $idea->id)->with('message', 'Your idea was updated!');
-});
 // Store
-Route::post('/ideas', function () {
+Route::post('/ideas', [IdeaController::class, 'store']);
 
-    Idea::create([
-        'description' => request('description'),
-        'state' => 'pending',
-    ]);
-
-    return redirect('/ideas')->with('message', 'Your idea was added!');
-});
 // Destroy
-Route::delete('/ideas/{idea}', function (Idea $idea) {
-
-    $idea->delete();
-
-    return redirect('/ideas')->with('message', 'Your idea was deleted!');
-});
+Route::delete('/ideas/{idea}', [IdeaController::class, 'destroy']);
