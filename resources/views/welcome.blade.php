684de880a1eb3fd34285be89fbddbfd0a7f8f75a
TeDDzTeR99 <myrkalol99@gmail.com>
2026-05-16 14:08:57 +0300

    Blade Directives Done

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,17 @@
 <x-layout title="Welcome">
         {{ $greeting }}, {{ $person }}!
         <h1>Welcome to Laravel!</h1>
+        @if(count($tasks))
+            <p> You have {{ count($tasks) }} tasks!</p>
+        @endif
+
+        @unless (count($tasks))
+            <p>You have no tasks!</p>
+        @endunless
+
+        @forelse ($tasks as $task)
+            <p>- {{ $task }}</p>
+        @empty
+            <p>No tasks to display.</p>
+        @endforelse
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'welcome', [ 
-    'greeting' => 'Hello',
-    'person' => request('person') ?? 'John Doe',
+Route::get('/', function(){
+    return view('welcome', [ 
+        'greeting' => 'Hello',
+        'person' => request('person') ?? 'John Doe',
+        'tasks' => [
+            'Go to the store',
+            'Finish my screencast',
+            'Clean the house',
+            'Make some food',
+        ]
 ]);
+});
 Route::view('/about', 'about');
 Route::view('/contact', 'contact');
